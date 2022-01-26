baa1f53786785d971add0fe52815382edd1b0507
Chris Vaillancourt <cvaillan22@gmail.com>
2022-01-25 20:14:41 -0800

    feat(register): store user info in db

--- src/accounts/register.js
+++ src/accounts/register.js
@@ -1,7 +1,17 @@
 import bcrypt from "bcryptjs";
 const { genSalt, hash } = bcrypt;
+
 async function registerUser(email, password) {
+  const { user } = await import("../user/user.js");
   const salt = await genSalt(10);
   const hashedPassword = await hash(password, salt);
+  const result = await user.insertOne({
+    email: {
+      address: email,
+      verified: false,
+    },
+    password: hashedPassword,
+  });
+  return result.insertedId;
 }
 export { registerUser };

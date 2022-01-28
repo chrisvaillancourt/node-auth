d0358c4e9936ec19964c14975a39c1aa1e377405
Chris Vaillancourt <cvaillan22@gmail.com>
2022-01-27 22:25:18 -0800

    feat(accounts): add log user in

---
+++ src/accounts/logUserIn.js
@@ -0,0 +1,13 @@
+import { createSession } from "./session.js";
+
+async function logUserIn(userId, request, reply) {
+  const connectionInformation = {
+    ip: request.ip,
+    userAgent: request.headers["user-agent"],
+  };
+  const sessionToken = await createSession(userId, connectionInformation);
+
+  // TODO complete login flow
+}
+
+export { logUserIn };

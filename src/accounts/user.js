bd88d69ead2a2487be94472bf57a874b7a68e5c6
Chris Vaillancourt <cvaillan22@gmail.com>
2022-01-31 22:39:37 -0800

    feat(accounts/user): lookup user from refresh tokens

--- src/accounts/user.js
+++ src/accounts/user.js
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken';
 import { ObjectId } from 'mongodb';
+import { createTokens } from './tokens.js';
 
 const JWT_SIGNAURE = process.env.JWT_SIGNATURE;
 
-async function getUserFromCookies(request) {
+async function getUserFromCookies(request, reply) {
   try {
     //  check if token exists
+    const { user } = await import('../user/user.js');
     if (request?.cookies?.accessToken) {
       // decode access token
       const {
@@ -13,31 +15,56 @@ async function getUserFromCookies(request) {
       } = request;
       const decodedAccessToken = jwt.verify(accessToken, JWT_SIGNAURE);
 
-      const { user } = await import('../user/user.js');
-
       return user.findOne({
         _id: ObjectId(decodedAccessToken?.userId),
       });
-    } else {
-      console.error('no access token');
     }
-    // TODO:
-    // get access and refresh tokens
-    // check if access token
-    // Decode access token
-    // return user from record
-    // Decode refresh token
-    // confirm session is valid
-    // check if valid session?
-    // look up user
-    // generatre refresh token
-    // return current user
-  } catch (error) {}
+    if (request?.cookies?.refreshToken) {
+      const {
+        cookies: { refreshToken },
+      } = request;
+      const { session } = await import('../session/session.js');
+      const { sessionToken } = jwt.verify(refreshToken, JWT_SIGNAURE);
+      const currentSession = await session.findOne({ sessionToken });
+      if (currentSession.valid) {
+        const currentUser = await user.findOne({
+          _id: ObjectId(currentSession.userId),
+        });
+        await refreshTokens(sessionToken, currentUser._id, reply);
+        return currentUser;
+      }
+    }
+  } catch (error) {
+    console.error(`There was an error getting the user from cookies: ${error}`);
+  }
 }
 
-async function refreshTokens() {
+async function refreshTokens(sessionToken, userId, reply) {
   try {
-  } catch (error) {}
+    // TODO refactor to fn and reuse in logUserIn.js
+    const { accessToken, refreshToken } = await createTokens(
+      sessionToken,
+      userId
+    );
+    const now = new Date();
+    const expires = now.setDate(now.getDate() + 30); // 30 days
+    reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        domain: 'localhost',
+        httpOnly: true,
+        // secure: true // requires https
+        expires,
+      })
+      .setCookie('accessToken', accessToken, {
+        path: '/',
+        domain: 'localhost',
+        httpOnly: true,
+        // secure: true // requires https
+      });
+  } catch (error) {
+    console.error(`There was an error setting refresh tokens: ${error}`);
+  }
 }
 
 export { getUserFromCookies, refreshTokens };

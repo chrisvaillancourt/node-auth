20506fbafff8810e3a267445368a7fbf1a3a043f
Chris Vaillancourt <cvaillan22@gmail.com>
2022-02-16 17:59:51 -0800

    refactor(accounts/log user in): make dry

--- src/accounts/logUserIn.js
+++ src/accounts/logUserIn.js
@@ -1,5 +1,5 @@
 import { createSession } from './session.js';
-import { createTokens } from './tokens.js';
+import { refreshTokens } from './user.js';
 
 async function logUserIn(userId, request, reply) {
   const connectionInformation = {
@@ -7,27 +7,7 @@ async function logUserIn(userId, request, reply) {
     userAgent: request.headers['user-agent'],
   };
   const sessionToken = await createSession(userId, connectionInformation);
-
-  const { accessToken, refreshToken } = await createTokens(
-    sessionToken,
-    userId
-  );
-  const now = new Date();
-  const expires = now.setDate(now.getDate() + 30); // 30 days
-  reply
-    .setCookie('refreshToken', refreshToken, {
-      path: '/',
-      domain: 'localhost',
-      httpOnly: true,
-      // secure: true // requires https
-      expires,
-    })
-    .setCookie('accessToken', accessToken, {
-      path: '/',
-      domain: 'localhost',
-      httpOnly: true,
-      // secure: true // requires https
-    });
+  await refreshTokens(sessionToken, userId, reply);
 }
 
 export { logUserIn, logUserIn as default };

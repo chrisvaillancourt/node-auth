3326c3041c742319968973a4fef3fec06da33b13
Chris Vaillancourt <cvaillan22@gmail.com>
2022-02-19 19:44:17 -0800

    feat: craft minimal request to parse verify route

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,9 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { Agent } from 'https';
 import { fastify } from 'fastify';
 import fastifyStatic from 'fastify-static';
+import fetch from 'cross-fetch';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +14,38 @@ async function startApp() {
   app.register(fastifyStatic, {
     root: join(__dirname, 'public'),
   });
+  app.get('/verify/:email/:token', {}, async (request, reply) => {
+    try {
+      const {
+        params: { email, token },
+      } = request;
+      const values = { email, token };
+
+      const httpsAgent = new Agent({
+        rejectUnauthorized: false,
+      });
+
+      const res = await fetch('https://api.nodeauth.dev/api/verify', {
+        method: 'POST',
+        credentials: 'include',
+        agent: httpsAgent,
+        body: JSON.stringify(values),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      console.log(res);
+      reply.code(200).send('all is well');
+    } catch (error) {
+      console.error('There was an error verifying the email: ', error);
+      reply.send({
+        data: {
+          status: 'FAILURE',
+        },
+      });
+    }
+  });
+
   await app.listen(PORT);
   console.log(`🚀 server listening on port ${PORT}`);
 }
